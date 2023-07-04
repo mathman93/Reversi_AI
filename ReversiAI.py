ee457a1897c778228e331ec8a8e29472ef115aa7
Timothy Anglea <mathman93@gmail.com>
2023-07-04 13:50:47 -0400

    starting positions

--- ReversiAI.py
+++ ReversiAI.py
@@ -6,16 +6,30 @@
 import math
 
 board_size = 8 # 8x8 board
-board_squares = board_size ** 2
-board_state = ""
 
 def main():
+    [black_stones, white_stones] = InitializeBoard()
     print("Starting Position:")
-    PrintBoard()
-    #print(board_squares)
-# End def main
+    PrintBoard(black_stones, white_stones)
+# End main
 
-def PrintBoard():
+# Sets initial configuration for the game board
+def InitializeBoard():
+    # Starting position for game
+    b = 0x0000001008000000
+    w = 0x0000000810000000
+    # basic check for board state
+    if (b & w > 0):
+        print("ERROR: two stones on same square")
+    # End if
+    return [b, w]
+# End def InitializeBoard
+
+# Displays current state of game board
+# Parameters
+#   b = int; bitmap for "black" stones on game board (X)
+#   w = int; bitmap for "white" stones on game board (O)
+def PrintBoard(b, w):
     square = 0
     for r in range(2*board_size + 1): # Row counter
         row_string = ""
@@ -24,8 +38,14 @@ def PrintBoard():
                 row_string += "-" # horizontal lines
             elif (c%2 == 0):
                 row_string += "|" # vertical lines
-            else:
-                row_string += " " # pieces
+            else: # game pieces
+                if ((b >> square) % 2== 1):
+                    row_string += "X" # black stone
+                elif ((w >> square) % 2 == 1):
+                    row_string += "O" # white stone
+                else:
+                    row_string += " " # no stones
+                # End if
                 square += 1
             # End if
         print(row_string)
