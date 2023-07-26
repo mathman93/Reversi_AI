36217ebc7ee5d0cde2ece5688e31ee965e316732
Timothy Anglea <mathman93@gmail.com>
2023-07-25 22:31:30 -0400

    Created board class

--- ReversiAI.py
+++ ReversiAI.py
@@ -3,26 +3,110 @@
 # Last Updated: 7/23/2023
 # Author: Timothy Anglea
 
-# Global Variables
-board_size = 8 # 8x8 board
-board_positions = [] # possible board selections
-row_names = ["A", "B", "C", "D", "E", "F", "G", "H"]
-column_names = ["1", "2", "3", "4", "5", "6", "7", "8"]
-for row in row_names:
-    for column in column_names:
-        board_positions.append(row+column)
-    # End for column
-# End for row
-board_coord = [] # possible board coordinates tuples
-for i in range(64):
-    board_coord.append((i//8, i%8))
-# End for i
+class GameBoard:
+    # Create game board variables and state
+    def __init__(self):
+        self.__board_size = 8 # Size of board (size x size)
+        self.board_positions = [] # possible board selections
+        self.__row_names = ["A", "B", "C", "D", "E", "F", "G", "H"]
+        self.__column_names = ["1", "2", "3", "4", "5", "6", "7", "8"]
+        for row in self.__row_names:
+            for column in self.__column_names:
+                self.board_positions.append(row+column)
+            # End for column
+        # End for row
+        # Initialize state of board
+        [self.black, self.white] = self.__InitializeBoard()
+
+    # End __init__
+
+    # Sets initial configuration for the game board
+    # Returns:
+    #   b = int; bit map of black stones on the board
+    #   w = int; bit map of white stones on the board
+    def __InitializeBoard(self):
+        # Starting position for game
+        w = 0x0000001008000000
+        b = 0x0000000810000000
+        # basic check for board state
+        if (b & w > 0):
+            print("ERROR: two stones on same square")
+        # End if
+        return [b, w]
+    # End InitializeBoard
+
+    # Displays current state of game board
+    def PrintBoard(self):
+        # Column Header
+        row_string = " "
+        for c in range(2*self.__board_size + 1):
+            if (c%2 == 1):
+                row_string += self.__column_names[c >> 1]
+            else:
+                row_string += "   " # Space
+            # End if
+        # End for c
+        print(row_string)
+        # Board layout
+        square = 0
+        for r in range(2*self.__board_size + 1): # Row counter
+            row_string = ""
+            if (r%2 == 1):
+                row_string += self.__row_names[r >> 1]
+            else:
+                row_string += " " # Space
+            # End if
+            for c in range(2*self.__board_size + 1): # Column counter
+                if (r%2 == 0):
+                    row_string += "--" # horizontal lines
+                elif (c%2 == 0):
+                    row_string += " | " # vertical lines
+                else: # game pieces
+                    if ((self.black >> square) % 2== 1):
+                        row_string += "X" # black stone
+                    elif ((self.white >> square) % 2 == 1):
+                        row_string += "O" # white stone
+                    else:
+                        row_string += " " # no stones
+                    # End if
+                    square += 1
+                # End if
+            # End for column
+            print(row_string)
+        # End for row
+        print("") # Space after board layout
+    # End PrintBoard
+
+    # Declares the winner of the game based on the total number of stones for each player
+    def DeclareWinner(self):
+        # Declare winner
+        [black_total, white_total] = self.CountStones()
+        print("Black: {0} - White: {1}".format(black_total, white_total))
+        if (black_total == white_total):
+            print("It's a tie! Good game.")
+        elif (black_total > white_total):
+            print("Black wins! Good game.")
+        else:
+            print("White wins! Good game.")
+        # End if
+    # End DeclareWinner
+
+    # Returns the number of pieces for each player
+    # Returns
+    #   black_count = int; Number of bits equal to 1 in self.black
+    #   white_count = int; Number of bits equal to 1 in self.white
+    def CountStones(self):
+        black_count = self.black.bit_count()
+        white_count = self.white.bit_count()
+        return [black_count, white_count]
+    # End CountStones
+# End class Board
 
 # Main function for playing game
 def main():
-    [black_stones, white_stones] = InitializeBoard() # Place starting stones
+    board = GameBoard() # Create game board for Reversi
     print("Starting Position:")
-    PrintBoard(black_stones, white_stones)
+    board.PrintBoard()
 
     # Make move until end of game
     bp = False
@@ -30,66 +114,50 @@ def main():
     while True:
         # Black's turn
         print("Black (X) to move...")
-        [black_stones, white_stones, bp] = Move(black_stones, white_stones)
-        PrintBoard(black_stones, white_stones)
+        [board.black, board.white, bp] = Move(board.black, board.white, board.board_positions)
+        board.PrintBoard()
 
         # End conditions
-        if (bp & wp):
+        if (bp & wp): # If both players recently passed
             break # Neither player can make a move
-        elif (white_stones == 0x0): # If white has no more stones
+        elif (board.white == 0x0): # If white has no more stones
             break # Black will win
-        elif (black_stones | white_stones == 0xffffffffffffffff):
+        elif (board.black | board.white == 0xffffffffffffffff): # If board is filled
             break # Black made final move
         # End if
         wp = False # Reset white pass flag
 
         # White's turn
         print("White (O) to move...")
-        [white_stones, black_stones, wp] = Move(white_stones, black_stones)
-        PrintBoard(black_stones, white_stones)
+        [board.white, board.black, wp] = Move(board.white, board.black, board.board_positions)
+        board.PrintBoard()
 
         # End conditions
-        if (bp & wp):
+        if (bp & wp): # If both players recently passed
             break # Neither player can make a move
-        elif (black_stones == 0x0): # If black has no more stones
+        elif (board.black == 0x0): # If black has no more stones
             break # White will win
-        elif (black_stones | white_stones == 0xffffffffffffffff):
+        elif (board.black | board.white == 0xffffffffffffffff): # If board is filled
             break # White made final move
         # End if
     # End while
     
     # Declare winner
-    [black_total, white_total] = CountStones(black_stones, white_stones)
-    print("Black: {0} - White: {1}".format(black_total, white_total))
-    if (black_total == white_total):
-        print("It's a tie! Good game.")
-    elif (black_total > white_total):
-        print("Black wins! Good game.")
-    else:
-        print("White wins! Good game.")
-    # End if
+    board.DeclareWinner()
 # End main
 
-# Sets initial configuration for the game board
+# Have player choose a move and update the state of the board
+# Parameters:
+#   player = int; current state of stones of player who is next to make a move
+#   opponent = int; current state of opponent's stones
+#   board_positions = list; all names of spaces on board
 # Returns:
-#   b = int; bit map of black stones on the board
-#   w = int; bit map of white stones on the board
-def InitializeBoard():
-    # Starting position for game
-    w = 0x0000001008000000
-    b = 0x0000000810000000
-    # basic check for board state
-    if (b & w > 0):
-        print("ERROR: two stones on same square")
-    # End if
-    return [b, w]
-# End InitializeBoard
+#   player = int; updated state of player's stones
+#   opponent = int; updated state of opponent's stones
+#   boolean; whether the player had to pass due to no possible valid moves (True = pass)
+def Move(player, opponent, board_positions):
 
-#   player = int; state of player's pieces who is next to make a move
-#   opponent = int; state of opponent's pieces
-def Move(player, opponent):
-
-    [valid_dictionary, valid_positions] = FindMoves(player, opponent)
+    [valid_dictionary, valid_positions] = FindMoves(player, opponent, board_positions)
     if (len(valid_positions) == 0):
         print("No valid moves: Pass")
         return [player, opponent, True] # No valid moves for player, and they must pass
@@ -126,7 +194,7 @@ def Move(player, opponent):
 
 # End Move
 
-def FindMoves(p, o):
+def FindMoves(p, o, board_positions):
     #valid_moves = [] # Set of valid moves that the player can make (NEEDED????)
     valid_move_dict = {} # Dictionary of valid moves and flipped spaces for valid moves
     #ps_index = [] # player_state_index
@@ -190,64 +258,6 @@ def FindMoves(p, o):
     valid_moves_list = [board_positions[x] for x in valid_move_dict.keys()]
     return [valid_move_dict, valid_moves_list]
 # End FindMoves
-
-# Returns the number of pieces for each player
-# Parameters
-#   b = int; bitmap for "black" stones on game board (X)
-#   w = int; bitmap for "white" stones on game board (O)
-# Returns
-#   black_count = int; Number of bits equal to 1 in b
-#   white_count = int; Number of bits equal to 1 in w
-def CountStones(b, w):
-    black_count = b.bit_count()
-    white_count = w.bit_count()
-    return [black_count, white_count]
-# End CountStones
-
-# Displays current state of game board
-# Parameters
-#   b = int; bitmap for "black" stones on game board (X)
-#   w = int; bitmap for "white" stones on game board (O)
-def PrintBoard(b, w):
-    # Column Header
-    row_string = " "
-    for c in range(2*board_size + 1):
-        if (c%2 == 1):
-            row_string += column_names[c >> 1]
-        else:
-            row_string += "   " # Space
-        # End if
-    # End for c
-    print(row_string)
-    # Board layout
-    square = 0
-    for r in range(2*board_size + 1): # Row counter
-        row_string = ""
-        if (r%2 == 1):
-            row_string += row_names[r >> 1]
-        else:
-            row_string += " " # Space
-        # End if
-        for c in range(2*board_size + 1): # Column counter
-            if (r%2 == 0):
-                row_string += "--" # horizontal lines
-            elif (c%2 == 0):
-                row_string += " | " # vertical lines
-            else: # game pieces
-                if ((b >> square) % 2== 1):
-                    row_string += "X" # black stone
-                elif ((w >> square) % 2 == 1):
-                    row_string += "O" # white stone
-                else:
-                    row_string += " " # no stones
-                # End if
-                square += 1
-            # End if
-        # End for column
-        print(row_string)
-    # End for row
-    print("") # Space after board layout
-# End PrintBoard
 
 ## Main Code
 main()
